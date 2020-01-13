d142031a01cb277fefea4932ebd3be1998f9e96d
Kovács Levente <kovacs.levente93@outlook.com>
2020-01-13 20:38:14 +0100

    Első commitok

---
+++ ReTek_CarMechanical/Interfaces/IWorksheet.cs
@@ -0,0 +1,18 @@
+﻿using Project_Retek_Store_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Retek_Store_Manager.Interface
+{
+    interface IWorksheet
+    {
+        bool UploadWorksheet(Worksheet worksheet);
+
+        bool GetSingleWorksheet(Worksheet worksheet);
+
+        bool GetAllWorksheet();
+    }
+}
---
+++ ReTek_CarMechanical/Models/Worksheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Retek_Store_Manager.Models
+{
+    class Worksheet
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public DateTime ExpectedEnd { get; set; }
+        public int KilometerState { get; set; }
+        public string UniqueId { get; set; }
+
+    }
+}
