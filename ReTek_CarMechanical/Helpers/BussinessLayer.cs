99b5cfb2a2264a415a4fffa57c0dc3b4cccb0852
Kovács Levente <kovacs.levente93@outlook.com>
2020-01-13 20:36:18 +0100

    Első commit

---
+++ ReTek_CarMechanical/Helpers/BussinessLayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReTek_CarMechanical.Helpers
+{
+    class BussinessLayer
+    {
+
+        private static BussinessLayer instance = null;
+        private static readonly object padlock = new object();
+
+        BussinessLayer()
+        {
+        }
+
+        public static BussinessLayer Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BussinessLayer();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public string Test()
+        {
+            return "Tesztszöveg a BussinessLayer-en keresztül";
+        }
+    }
+}
+
---
+++ ReTek_CarMechanical/ViewModels/MainWindowsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReTek_CarMechanical.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReTek_CarMechanical.ViewModels
+{
+    class MainWindowsViewModel
+    {
+        public string Test { get { return BussinessLayer.Instance.Test(); } }
+    }
+}
