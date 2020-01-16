1880b9400d7ea6a9fc7791427255614071fd9448
Varga Eszter <v.eszter1992@gmail.com>
2020-01-16 22:39:56 +0100

    GetAllCarByUserTest

--- ReTek_CarMechanicalTests/Helpers/BusinessLayerTests.cs
+++ ReTek_CarMechanicalTests/Helpers/BusinessLayerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ReTek_CarMechanical.Helpers;
+using ReTek_CarMechanical.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,23 @@ namespace ReTek_CarMechanical.Helpers.Tests
             var allClients = BusinessLayer.Instance.GetAllClient();
             Assert.IsNotNull(allClients);
             Assert.IsTrue(allClients.Count() > 0);
+        }
+
+        [TestMethod()]
+        public void GetAllCarByUserTest()
+        {
+            var clients = BusinessLayer.Instance.GetAllClient();
+            Client whosecar = new Client();
+            foreach (var item in clients)
+            {
+                if (item.FirstName == "Varga")
+                {
+                    whosecar = item;
+                }
+            }
+            var cars = BusinessLayer.Instance.GetAllCarByUser(whosecar);
+            Assert.IsNotNull(cars);
+            Assert.IsTrue(cars.Count() > 0);
         }
     }
 }
